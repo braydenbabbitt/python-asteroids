21557c52745a3fed0b7131d3891f5b6079650052
Brayden Babbitt <1495342+braydenbabbitt@users.noreply.github.com>
2025-09-01 10:42:07 -0600

    refactor: added groups

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
-from constants import SCREEN_WIDTH, SCREEN_HEIGHT
+
+from constants import SCREEN_HEIGHT, SCREEN_WIDTH
 from player import Player
 
 
@@ -8,14 +9,19 @@ def main():
     clock = pygame.time.Clock()
     dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
-        player.draw(screen)
-        player.update(dt)
+        for d in drawable:
+            d.draw(screen)
+        updatable.update(dt)
         pygame.display.flip()
         dt = clock.tick(60)
 
